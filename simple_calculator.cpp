b2a9eba29571d14dd12c6f3b238d9cc070e8664a
NIKETA21 <84794703+NIKETA21@users.noreply.github.com>
2022-02-23 19:34:27 +0530

    Update simple_calculator.cpp

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -3,32 +3,32 @@
 using namespace std;
 
 int main( )
-{
-char op;
-float num1,num2;
-cout<<"enter an operator (+,-,*,/): ";
-cin>>op;
-cout<<"enter two numbers: "<<endl;
-cin>>num1>>num2;
+    {
+    char op;
+    float num1,num2;
+    cout<<"enter an operator (+,-,*,/): ";
+    cin>>op;
+    cout<<"enter two numbers: "<<endl;
+    cin>>num1>>num2;
 
 switch(op) //using switch operation we  get operator  and value of two number from user, and  perform operation 
-{
-case'+':
-cout<<num1<<"+"<<num2<<"="<<num1+num2;
-break;
-case'-':
-cout<<num1<<"+"<<num2<<"="<<num1-num2;
-break;
-case'*':
-cout<<num1<<"+"<<num2<<"="<<num1*num2;
-break;
-case'/':
-cout<<num1<<"+"<<num2<<"="<<num1/num2;
-break;
+    {
+      case'+':
+      cout<<num1<<"+"<<num2<<"="<<num1+num2;
+      break;
+      case'-':
+      cout<<num1<<"+"<<num2<<"="<<num1-num2;
+      break;
+      case'*':
+      cout<<num1<<"+"<<num2<<"="<<num1*num2;
+      break;
+      case'/':
+      cout<<num1<<"+"<<num2<<"="<<num1/num2;
+      break;
 
-default:
-cout<<"operator not matched";
-break;
-}
-return 0;
-}
+      default:
+      cout<<"operator not matched";
+      break;
+      }
+    return 0;
+    }
