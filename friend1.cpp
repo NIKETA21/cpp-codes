ab7550feb5a51222fc734db68138c88d22fb7763
NIKETA21 <84794703+NIKETA21@users.noreply.github.com>
2022-02-23 19:31:18 +0530

    Update friend1.cpp

--- friend1.cpp
+++ friend1.cpp
@@ -1,26 +1,27 @@
 #include<iostream>
 class numbers
-{
-int num1,num2;
-public :
-void setdata(int a,int b);
-friend int add(numbers N);
-};
+    {
+    int num1,num2;
+    public :
+    void setdata(int a,int b);
+    friend int add(numbers N);
+    };
 
 void numbers:: setdata(int a, int b) //function call using friend
-{
-num1=a;
-num2=b;
-}
+    {
+    num1=a;
+    num2=b;
+    }
 
 int add (numbers N)
-{ return (N.num1+N.num2); //adding two numbers
-}
+    { 
+    return (N.num1+N.num2); //adding two numbers
+    }
 
 int main()
-{
-numbers N1;
-N1.setdata(10,20);
-cout<<"sum = "<<add(N1); //getting sum of two numbers
-return 0;
-}
+  ` {
+    numbers N1;
+    N1.setdata(10,20);
+    cout<<"sum = "<<add(N1); //getting sum of two numbers
+    return 0;
+    }
